2ab2e117e00d85ac5d79133832bf8d632a99fc48
hectorlavado99 <58657138+hectorlavado99@users.noreply.github.com>
2020-05-09 21:01:50 +0200

    update

--- index.php
+++ index.php
@@ -3,23 +3,14 @@ require_once "vendor/autoload.php";
 
 $faker = Faker\Factory::create();
 
-
-//$dateFaker = Faker\Provider\DateTime;
-
 for($i=0; $i<1000; $i++){
-//echo $faker->name;
-//echo $faker->address;
-//echo $faker->city;
-//echo $faker->email;
-
+echo $faker->name;
+echo $faker->address;
+echo $faker->city;
+echo $faker->email;
+echo $faker->unique()->dateTimeBetween('-30 years', 'now')->format('Y-m-d');
+echo $faker->numberBetween($min = 0, $max = 10000);
 }
 
-echo $faker->unique()->dateTimeBetween('-30 years', 'now')->format('Y-m-d');
-//echo $faker->unique()->dateTimeBetween($startDate = '-30 years', $endDate = 'now', $timezone = null);
-
-//echo $faker->date;
-
-//
- // DateTime('2003-03-15 02:00:49', 'Africa/Lagos')
 
 ?>
