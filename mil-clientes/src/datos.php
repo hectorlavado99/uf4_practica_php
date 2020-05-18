724b2b1222d9905d3e81316e9d541bc48ee5963c
Ayuso <gokly500@gmail.com>
2020-05-18 17:00:44 +0200

    composer

---
+++ mil-clientes/src/datos.php
@@ -0,0 +1,19 @@
+<?php
+class Datos{
+    function faker(){
+        require_once "vendor/autoload.php";
+
+        $faker = Faker\Factory::create();
+        $api = new ApiCliente();
+        for($i=0; $i<1000; $i++){
+        $name = $faker->name;
+        $address=  $faker->address;
+        $city = $faker->city;
+        $email = $faker->email;
+        $date = $faker->unique()->dateTimeBetween('-30 years', 'now')->format('Y-m-d');
+        
+        $api->insertarCliente($name,$address,$city,$email,$date);
+        }
+    }
+}
+?>
